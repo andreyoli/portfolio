1f18607e4388b1e87b60cf06d5818c59cba04a87
Andrey <andrey.o.paula@gmail.com>
2021-04-11 19:08:47 -0300

    Add secondary color in themes

--- src/styles/themes/dark.ts
+++ src/styles/themes/dark.ts
@@ -1,15 +1,16 @@
 import defaultValues from './default'
 
 export default {
-  title: 'dark',
+	title: 'dark',
 
-  colors: {
-    background: '#181818',
-    text: '#EDEDED',
-    primary: '#7A00E5',
-    gradient:
-      'linear-gradient(180deg, rgba(122, 0, 230, 0.7) 0%, rgba(128, 68, 0, 0.7) 100%)'
-  },
+	colors: {
+		background: '#181818',
+		text: '#EDEDED',
+		primary: '#7A00E5',
+		secondary: '#e57a00',
+		gradient:
+			'linear-gradient(180deg, rgba(122, 0, 230, 0.7) 0%, rgba(128, 68, 0, 0.7) 100%)'
+	},
 
-  ...defaultValues
+	...defaultValues
 }
--- src/styles/themes/light.ts
+++ src/styles/themes/light.ts
@@ -1,15 +1,16 @@
 import defaultValues from './default'
 
 export default {
-  title: 'light',
+	title: 'light',
 
-  colors: {
-    background: '#F3F4F7',
-    text: '#1F1F1F',
-    primary: '#7A00E5',
-    gradient:
-      'linear-gradient(180deg, rgba(122, 0, 230, 0.7) 0%, rgba(128, 68, 0, 0.7) 100%)'
-  },
+	colors: {
+		background: '#F3F4F7',
+		text: '#1F1F1F',
+		primary: '#7A00E5',
+		secondary: '#e57a00',
+		gradient:
+			'linear-gradient(180deg, rgba(122, 0, 230, 0.7) 0%, rgba(128, 68, 0, 0.7) 100%)'
+	},
 
-  ...defaultValues
+	...defaultValues
 }
